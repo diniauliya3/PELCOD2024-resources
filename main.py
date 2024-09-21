b61678b1406cd053367d9af89d466b423c946bad
diniauliya3 <diniauliyamulha@gmail.com>
2024-09-21 15:28:53 +0700

    1main.py

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-# for loop
+nilai = int(input("masukan nilai : "))
+print(nilai)
+
+nilai = nilai + 5
+print(nilai)
+
+if (nilai  > 90 and nilai <= 100):
+    print("selamat anda lulus,  dengan nilai memuaskan")
+elif (nilai > 80):
+    print("selamat anda lulus")
+elif (nilai > 70):
+    print("selamat tapi masuk jurang")
+else:
+    print("anda masih belum lulus tes, semangat dan coba lagi")
 #for i in range(1, 10, 2):
 #    print(1)
 #    print("hello future")
