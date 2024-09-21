e0200ebcd99910abd4cf93fe190765402aa5dd05
diniauliya3 <diniauliyamulha@gmail.com>
2024-09-21 15:15:47 +0700

    pelatihan coding  21 sep 24, bismillah calon programmer handal

---
+++ main.py
@@ -0,0 +1,16 @@
+# for loop
+#for i in range(1, 10, 2):
+#    print(1)
+#    print("hello future")
+
+#angka = 30
+
+# while(angka > 1):
+#    print(angka)
+
+def sayHello():
+    print("hello jaemin")
+    print("ily")
+
+sayHello()
+sayHello()
